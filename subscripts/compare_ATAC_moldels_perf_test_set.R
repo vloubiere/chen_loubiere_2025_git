5dc4bc426b628ec3ce3f33af969c51c455ae20d0
vloubiere <vincent.loubiere@imp.ac.at>
2026-07-22 13:04:13 +0200

    clean unused scripts

--- subscripts/compare_ATAC_moldels_perf_test_set.R
+++
@@ -1,7 +0,0 @@
-setwd("/groups/stark/vloubiere/projects/DeepATAC_shenzhi/")
-require(vlfunctions)
-
-# Import data ----
-dat <- readRDS("db/predictions/model1_ATAC_mean_predictions_test_set.rds")
-
-#
